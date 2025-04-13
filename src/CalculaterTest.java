87edceb6230ab86a3e9fd1ec33f7af1978f6a51c
OrandolY <845865588@qq.com>
2025-04-13 21:14:47 +0800

    reflect String

--- src/CalculaterTest.java
+++ src/CalculaterTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.*;
 
 public class CalculaterTest {
     @Test
-    public void shouldReturn2When1Plus1() {
+    public void shouldReturn2When1Plus1() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculater calculater = new Calculater();
-        int actualResult = calculater.compute(1, 1, '+');
+        int actualResult = calculater.compute(1, 1, "Add");
         int expectResult = 2;
         Assert.assertEquals(expectResult, actualResult);
     }
@@ -15,9 +15,9 @@ public class CalculaterTest {
 
 
     @Test
-    public void shouldReturn0When1Sub1() {
+    public void shouldReturn0When1Sub1() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculater calculater = new Calculater();
-        int actualResult = calculater.compute(1, 1, '-');
+        int actualResult = calculater.compute(1, 1, "Sub");
         int expectResult = 0;
         Assert.assertEquals(expectResult, actualResult);
     }
